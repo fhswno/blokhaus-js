c3412f4bb56216a5a719a0f82b94ae250950fa15
Dave Ohayon <davidohn.uk@hotmail.com>
2026-03-07 22:59:15 +0100

    mdx components hook

---
+++ apps/docs/mdx-components.tsx
@@ -0,0 +1,10 @@
+// FUMADOCS
+import defaultMdxComponents from "fumadocs-ui/mdx";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function useMDXComponents(components?: any): any {
+  return {
+    ...defaultMdxComponents,
+    ...components,
+  };
+}
