38c8b56e25ce64097804ecdd14d5a681dd3309c1
Dave Ohayon <davidohn.uk@hotmail.com>
2026-03-08 02:16:24 +0100

    full rebrand

---
+++ packages/blokhaus/src/plugins/ListPlugin.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { registerList, registerCheckList } from "@lexical/list";
+
+/**
+ * Registers @lexical/list behaviour: Enter on an empty list item exits the list,
+ * Tab/Shift-Tab for indent/outdent, and other list integrity transforms.
+ * Also registers check list behaviour: click/spacebar to toggle checkbox state.
+ */
+export function ListPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    const unregisterList = registerList(editor);
+    const unregisterCheckList = registerCheckList(editor);
+    return () => {
+      unregisterList();
+      unregisterCheckList();
+    };
+  }, [editor]);
+
+  return null;
+}
