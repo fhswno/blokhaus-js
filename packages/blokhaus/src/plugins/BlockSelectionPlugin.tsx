c8ce8f74fddd8280bf32896ba093d853273f6f51
Dave Ohayon <davidohn.uk@hotmail.com>
2026-03-08 02:16:02 +0100

    full rebrand

---
+++ packages/blokhaus/src/plugins/BlockSelectionPlugin.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { createPortal } from "react-dom";
+import { useBlockHighlight } from "../hooks/useBlockHighlight";
+import { useMarqueeSelect } from "../hooks/useMarqueeSelect";
+import { useBlockSelectionActions } from "../hooks/useBlockSelectionActions";
+
+export function BlockSelectionPlugin() {
+  const { clearHighlights, hasBlockSelectionRef, isDraggingRef } =
+    useBlockHighlight();
+  const { marqueeRect } = useMarqueeSelect(hasBlockSelectionRef, isDraggingRef);
+  useBlockSelectionActions(hasBlockSelectionRef, isDraggingRef, clearHighlights);
+
+  if (!marqueeRect || typeof document === "undefined") return null;
+
+  return createPortal(
+    <div
+      data-testid="block-selection-marquee"
+      style={{
+        position: "fixed",
+        top: `${marqueeRect.top}px`,
+        left: `${marqueeRect.left}px`,
+        width: `${marqueeRect.width}px`,
+        height: `${marqueeRect.height}px`,
+        backgroundColor:
+          "var(--blokhaus-block-selection, rgba(45, 133, 255, 0.08))",
+        border: "1px solid rgba(45, 133, 255, 0.25)",
+        borderRadius: "3px",
+        pointerEvents: "none",
+        zIndex: 9999,
+      }}
+    />,
+    document.body,
+  );
+}
